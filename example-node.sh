2ef4c1b8fa09c50d27a61c6c7331054968a71858
Petri Sirkkala <sirpete@iki.fi>
2015-02-11 13:29:22 +0200

    Try harder to avoid bower interactive prompt

--- example-node.sh
+++ example-node.sh
@@ -6,27 +6,31 @@ if [[ $(docker info) != *"Kernel Version:"* ]]; then
 fi
 
 PROJECT=docker-builder-pattern
+WORKDIR=ui-select
 
 read -r -d '' SCRIPT <<- End
+    cd $PROJECT
     Xvfb :1 -ac &
     export DISPLAY=:1
 
-    [[ -d $PROJECT/ui-select ]] || git clone https://github.com/angular-ui/ui-select.git $PROJECT/ui-select
+    if [[ ! -d $WORKDIR ]]; then
+         git clone https://github.com/angular-ui/ui-select.git $WORKDIR
+    fi
 
-    cd $PROJECT/ui-select
+    cd $WORKDIR
 
     npm install -g bower gulp
-    npm install && bower install
+    npm install --config.interactive=false && bower install --config.interactive=false
     gulp
 End
 
 # https://github.com/sirkkalap/jenkins-swarm-slave-nlm-docker
-IMG=sirkkalap/jenkins-swarm-slave-nlm
+IMG=sirkkalap/jenkins-swarm-slave-nlm:java7
 MOUNT="-v $(pwd):/home/jenkins-slave/$PROJECT"
 NAME="--name $PROJECT"
 OPTS="-it --sig-proxy=true"
 
-VOLFROM=$(docker ps -a | grep $PROJECT-volume | cut -d ' ' -f1)
+VOLFROM=$(docker ps -a | grep -o $PROJECT-volume)
 if [ ! -z $VOLFROM ]; then
     VOLFROM="--volumes-from $VOLFROM"
 else
