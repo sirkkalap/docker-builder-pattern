1424594814e4ffcd213ec8149e8cec9963dbaaac
Petri Sirkkala <sirpete@iki.fi>
2015-02-24 01:48:51 +0200

    Debug Chrome failure

--- example/node/build.sh
+++ example/node/build.sh
@@ -9,9 +9,15 @@ PROJECT=ui-select
 CONTAINER_NAME=ui-select-build
 
 read -r -d '' SCRIPT <<- End
+	sudo apt-get -y install imagemagick
     cd $PROJECT
     sudo Xvfb :1 -ac &
     export DISPLAY=:1
+    google-chrome about:blank &
+    firefox about:blank &
+    sleep 3
+    import -window root -display :1 -screen 0.png
+    exit 1
 
     sudo npm install -g bower gulp
     sudo npm install && bower install
